7020f4101626aa0f2794f815c84488de449aee0d
SalumMax <salumaxx@gmail.com>
2024-08-18 19:48:33 +1000

    add ability to replace photos

--- property-pulse/app/actions/editProperty.js
+++ property-pulse/app/actions/editProperty.js
@@ -23,6 +23,38 @@ const editProperty = async (propertyId, formData) => {
     throw new Error("Current user is not authorised to edit this property");
   }
 
+  const images = formData.getAll("images");
+  let imageUrls = existingProperty.images;
+
+  if (images.length > 0) {
+    // Delete old images from Cloudinary
+    for (const imgUrl of imageUrls) {
+      const publicId = imgUrl.split('/').pop().split('.')[0];
+      await cloudinary.uploader.destroy(publicId);
+    }
+
+    // Upload new images to Cloudinary
+    imageUrls = [];
+    for (const imageFile of images) {
+      const imageBuffer = await imageFile.arrayBuffer();
+      const imageArray = Array.from(new Uint8Array(imageBuffer));
+      const imageData = Buffer.from(imageArray);
+
+      // Convert to base64
+      const imageBase64 = imageData.toString("base64");
+
+      // Make request to Cloudinary
+      const result = await cloudinary.uploader.upload(
+        `data:image/png;base64,${imageBase64}`,
+        {
+          folder: "property-flow",
+        }
+      );
+
+      imageUrls.push(result.secure_url);
+    }
+  }
+
   const propertyData = {
     owner: userId,
     type: formData.get("type"),
@@ -38,6 +70,7 @@ const editProperty = async (propertyId, formData) => {
     baths: formData.get("baths"),
     square_feet: formData.get("square_feet"),
     amenities: formData.getAll("amenities"),
+    images: imageUrls,
     rates: {
       nightly: formData.get("rates.nightly"),
       weekly: formData.get("rates.weekly"),
